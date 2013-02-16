3b1c6eef1b4d9905e8562207ab38c4021660f86f
Paul Tagliamonte <paultag@debian.org>
2013-02-15 23:49:04 -0500

    Adapting for APIv1

--- atlantic/actions.py
+++ atlantic/actions.py
@@ -1,50 +1,7 @@
-from atlantic.utils import load_config, api_call
+from atlantic.utils import api_call
 import datetime as dt
 import json
 
 
 def ping():
-    obj = load_config()
-    return api_call(obj, '/ping', {
-        'node': obj['node_name']
-    })
-
-
-def finish(job_id):
-    obj = load_config()
-    return api_call(obj, '/finish', {
-        'node': obj['node_name'],
-        'job': job_id
-    })
-
-
-def result(report):
-    obj = load_config()
-    return api_call(obj, '/result', {
-        'node': obj['node_name'],
-        'data': json.dumps(report)
-    })
-
-
-def aquire():
-    obj = load_config()
-    return api_call(obj, '/aquire', {
-        'node': obj['node_name']
-    })
-
-
-def echo(data):
-    obj = load_config()
-    return api_call(obj, '/echo', {
-        'node': obj['node_name'],
-        'time': dt.datetime.now()
-    })
-
-
-def package(package_id):
-    obj = load_config()
-    return api_call(
-        obj, "/package/%s" % (package_id),
-        {},
-        do_sign=False
-    )
+    return api_call('/ping', {})
--- atlantic/utils.py
+++ atlantic/utils.py
@@ -14,29 +14,34 @@ def load_config():
 
 
 def post(url, data):
-    print url
-    return urllib2.urlopen(url, urllib.urlencode(data))
+    try:
+        return urllib2.urlopen(url, urllib.urlencode(data))
+    except urllib2.HTTPError as e:
+        print url, data
+        print e.read()
+        raise
 
 
-def api_call(obj, url, data, do_sign=True):
+def api_call(url, data, do_sign=True):
+    obj = load_config()
+    data['name'] = obj['node_name']
     if do_sign:
         tok = token()['token']
-        signature = sign(tok, obj)
+        signature = sign(tok, obj['private_key'])
         data['signature'] = signature
-    return json.load(post("%s%s" % (obj['api_base'], url), data))
+    ret = json.load(post("%s%s" % (obj['api_base'], url), data))
+    return ret
 
 
 def sign(string, obj):
-    s = "%s-%s" % (obj['private_key'], string)
+    s = "%s-%s" % (obj, string)
     return hashlib.sha256(s).hexdigest()
 
 
-def token(obj=None):
-    if obj is None:
-        obj = load_config()
-
-    return api_call(obj, '/token', {
-        'node': obj['node_name'],
+def token():
+    obj = load_config()
+    return api_call('/token', {
+        'name': obj['node_name'],
     }, do_sign=False)
 
 
